ef6697343a44951309b5a1b21a2a5d751a3f4d6f
wangzhenpeng <wangzhenpeng@gomeplus.com>
2017-09-19 15:42:48 +0800

    commit

---
+++ chapter1-1-2/src/main/java/leaning/lock/Chapter112Application.java
@@ -0,0 +1,12 @@
+package leaning.lock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chapter112Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chapter112Application.class, args);
+	}
+}
