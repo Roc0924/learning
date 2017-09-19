007d2666cd34c65b970a15cd3ab96af755b8de5c
wangzhenpeng <wangzhenpeng@gomeplus.com>
2017-09-19 15:03:18 +0800

    commit

---
+++ chapter1-1-1/src/main/java/learning/jdk/collections/LearningHashMap.java
@@ -0,0 +1,22 @@
+package learning.jdk.collections;
+
+import java.util.HashMap;
+
+/**
+ * Create with IntelliJ IDEA
+ * Author               : wangzhenpeng
+ * Date                 : 2017/7/31
+ * Time                 : 14:15
+ * Description          :
+ */
+public class LearningHashMap {
+    public static void main(String[] args) {
+        HashMap<String, String> hashMap = new HashMap<>();
+        hashMap.put("key1", "value1");
+
+        System.out.println(hashMap);
+
+
+    }
+
+}
