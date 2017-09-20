90d53a7b94f6d4f949708a46253ddf7d11c7a45a
wangzhenpeng <wangzhenpeng@gomeplus.com>
2017-09-20 14:20:03 +0800

    redis实现分布式锁

--- chapter1-1-2/src/main/java/leaning/lock/lock/LockAspect.java
+++ chapter1-1-2/src/main/java/leaning/lock/lock/LockAspect.java
@@ -2,10 +2,15 @@ package leaning.lock.lock;
 
 import leaning.lock.annotation.RedisLockAnno;
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.Signature;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 /**
@@ -23,11 +28,16 @@ public class LockAspect {
     RedisLock redisLock;
 
     @Around("@annotation(redisLockAnno)")
-    public Object test(final ProceedingJoinPoint pjp, RedisLockAnno redisLockAnno) {
+    public Object test(final ProceedingJoinPoint pjp, RedisLockAnno redisLockAnno) throws InterruptedException {
         Object result = null;
-        Object[] params = pjp.getArgs();
         System.out.println("start");
-//        redisLock.lock()
+        Map<String, Object> paramsMap = generateParamsMap(pjp);
+        if(redisLock.lock(paramsMap.get("sku").toString(), 300000L)){
+            System.out.println(Thread.currentThread().getName() + " locked " + paramsMap.get("sku"));
+        } else {
+            System.out.println(Thread.currentThread().getName() + " timeout");
+            return null;
+        }
         try {
             result = pjp.proceed();
         } catch (Exception e) {
@@ -35,10 +45,29 @@ public class LockAspect {
         } catch (Throwable throwable) {
             throwable.printStackTrace();
         } finally {
-            long end = System.currentTimeMillis();
+            redisLock.unLock(paramsMap.get("sku").toString());
+            System.out.println(Thread.currentThread().getName() + " unlocked " + paramsMap.get("sku"));
         }
 
         System.out.println("end");
         return result;
     }
+
+    private Map<String, Object> generateParamsMap(ProceedingJoinPoint pjp) {
+        Object[] paramsValue = pjp.getArgs();
+        Signature signature = pjp.getSignature();
+        MethodSignature methodSignature = (MethodSignature)signature;
+        String[] paramsName = methodSignature.getParameterNames();
+
+        HashMap<String, Object> paramsMap = new HashMap<>();
+        for (int i = 0; i < paramsName.length; i++) {
+            if (paramsName[i].toLowerCase().contains("password")) {
+                continue;
+            }
+            paramsMap.put(paramsName[i], paramsValue[i]);
+        }
+        return paramsMap;
+    }
+
+
 }
