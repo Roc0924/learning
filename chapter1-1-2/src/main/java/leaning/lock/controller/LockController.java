ebebe225fa599a3531073f1034d794035ef8c255
wangzhenpeng <wangzhenpeng@gomeplus.com>
2017-09-20 17:03:48 +0800

    动态代理

--- chapter1-1-2/src/main/java/leaning/lock/controller/LockController.java
+++ chapter1-1-2/src/main/java/leaning/lock/controller/LockController.java
@@ -3,14 +3,10 @@ package leaning.lock.controller;
 import leaning.lock.service.RedisLockService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
-import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.annotation.Resource;
-import javax.annotation.Resources;
 
 /**
  * Create with IntelliJ IDEA
