c180fbf7737eed13e552f58121d3d1517c55ad70
Roc0924 <1228916292@qq.com>
2017-09-29 15:08:29 +0800

    新建模块spring transaction

---
+++ chapter1-1-5/src/main/java/leaning/transactional/service/TransactionalService.java
@@ -0,0 +1,14 @@
+package leaning.transactional.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Create with IntelliJ IDEA
+ * Author               : wangzhenpeng
+ * Date                 : 2017/9/29
+ * Time                 : 15:05
+ * Description          : transactional service 类
+ */
+@Service("transactionalService")
+public class TransactionalService {
+}
