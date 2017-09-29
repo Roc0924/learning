5237b6d1e9db94c9ca1617e2493203673aef3d7d
Roc0924 <1228916292@qq.com>
2017-09-29 14:45:52 +0800

    新建模块spring transaction

---
+++ chapter1-1-5/src/main/java/leaning/transaction/Chapter115Application.java
@@ -0,0 +1,12 @@
+package leaning.transaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chapter115Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chapter115Application.class, args);
+	}
+}
