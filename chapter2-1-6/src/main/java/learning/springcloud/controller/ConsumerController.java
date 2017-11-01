4da85104697a67f46eadc5ea82136fd1936ffe42
Roc0924 <1228916292@qq.com>
2017-11-01 13:59:53 +0800

    Ribbon实现服务调用和负载均衡

---
+++ chapter2-1-6/src/main/java/learning/springcloud/controller/ConsumerController.java
@@ -0,0 +1,27 @@
+package learning.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * Create with IntelliJ IDEA
+ * Author               : wangzhenpeng
+ * Date                 : 2017/11/1
+ * Time                 : 13:54
+ * Description          : 消费者
+ */
+@RestController
+public class ConsumerController {
+
+    @Autowired
+    RestTemplate restTemplate;
+
+    @RequestMapping(value = "/add", method = RequestMethod.GET)
+    public String add() {
+        return restTemplate.getForEntity("http://COMPUTE-SERVICE/add?a=10&b=20", String.class).getBody();
+    }
+
+}
