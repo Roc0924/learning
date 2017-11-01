e9626b9faf7978ee2ad44658789b1cf8ed1c9c15
Roc0924 <1228916292@qq.com>
2017-11-01 14:23:26 +0800

    Feign实现服务调用和负载均衡

---
+++ chapter2-1-7/src/main/java/learning/springcloud/Chapter217Application.java
@@ -0,0 +1,12 @@
+package learning.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chapter217Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chapter217Application.class, args);
+	}
+}
