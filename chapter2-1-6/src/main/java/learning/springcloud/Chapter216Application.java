b73e2d6438a413027fefb922b59c3201e12ae2dd
Roc0924 <1228916292@qq.com>
2017-11-01 11:21:50 +0800

    Ribbon实现服务调用和负载均衡

---
+++ chapter2-1-6/src/main/java/learning/springcloud/Chapter216Application.java
@@ -0,0 +1,12 @@
+package learning.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chapter216Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chapter216Application.class, args);
+	}
+}
