a92cea0486a2cb1f9684db951bed0dedc0a9fc35
Roc0924 <1228916292@qq.com>
2017-11-23 10:42:52 +0800

    kotlin

---
+++ chapter3-1-1/HelloWorld/src/HelloWorld/ReadManyNamesFromCommandLine.kt
@@ -0,0 +1,14 @@
+package HelloWorld
+
+/**
+ * Create with IntelliJ IDEA
+ * Author               : wangzhenpeng
+ * Date                 : 2017/11/7
+ * Time                 : 20:42
+ * Description          :
+ */
+fun main(args: Array<String>) {
+    for (name in args) {
+        println("Hello $name")
+    }
+}
