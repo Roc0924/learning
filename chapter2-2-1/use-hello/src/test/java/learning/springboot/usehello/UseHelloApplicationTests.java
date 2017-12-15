d4d02adc01719fbc0f12fd4f55d41d68f79f2a20
Roc0924 <1228916292@qq.com>
2017-12-15 14:04:05 +0800

    spring boot自动化配置

---
+++ chapter2-2-1/use-hello/src/test/java/learning/springboot/usehello/UseHelloApplicationTests.java
@@ -0,0 +1,16 @@
+package learning.springboot.usehello;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UseHelloApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
