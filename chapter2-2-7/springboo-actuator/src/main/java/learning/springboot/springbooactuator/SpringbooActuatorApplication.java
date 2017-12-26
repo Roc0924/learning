c7791450483523c69af92fd34ab5f2bb29db7b63
Roc0924 <1228916292@qq.com>
2017-12-26 11:23:44 +0800

    spring boot actuator

---
+++ chapter2-2-7/springboo-actuator/src/main/java/learning/springboot/springbooactuator/SpringbooActuatorApplication.java
@@ -0,0 +1,12 @@
+package learning.springboot.springbooactuator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbooActuatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbooActuatorApplication.class, args);
+	}
+}
---
+++ chapter2-2-7/springboo-actuator/src/test/java/learning/springboot/springbooactuator/SpringbooActuatorApplicationTests.java
@@ -0,0 +1,16 @@
+package learning.springboot.springbooactuator;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbooActuatorApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
