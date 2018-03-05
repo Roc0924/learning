e186e68a44a002c47c34b794f67587d2b20f132a
Roc0924 <1228916292@qq.com>
2018-03-05 18:24:12 +0800

    stripping install chaincode

--- chapter3-7/fabric-admin/src/main/java/learning/chaincode/fabricadmin/services/FabricService.java
+++ chapter3-7/fabric-admin/src/main/java/learning/chaincode/fabricadmin/services/FabricService.java
@@ -22,6 +22,7 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
+import static java.lang.String.format;
 import static java.nio.charset.StandardCharsets.UTF_8;
 
 /**
@@ -224,12 +225,77 @@ public class FabricService {
     public ChainCodeDto upgradeChainCode(ChaincodeID chaincodeID, SampleOrg sampleOrg) {
         ChainCodeDto installedChainCodeDto = queryInstalledChainCodeByName(chaincodeID.getName());
         try {
+
+
+            InstallProposalRequest installProposalRequest = hfClient.newInstallProposalRequest();
+            installProposalRequest.setChaincodeID(chaincodeID);
+            ////For GO language and serving just a single user, chaincodeSource is mostly likely the users GOPATH
+            installProposalRequest.setChaincodeSourceLocation(new File(config.getFixturesPath()));
+            installProposalRequest.setChaincodeVersion(chaincodeID.getVersion());
+            installProposalRequest.setProposalWaitTime(Integer.parseInt(config.getProposalWaitTime()));
+
+
+            ////////////////////////////
+            // only a client from the same org as the peer can issue an install request
+            int numInstallProposal = 0;
+
+            Collection<ProposalResponse> responses;
+            final Collection<ProposalResponse> successful = new LinkedList<>();
+            final Collection<ProposalResponse> failed = new LinkedList<>();
+            Collection<Peer> peersFromOrg = channel.getPeers();
+            numInstallProposal = numInstallProposal + peersFromOrg.size();
+
+            responses = hfClient.sendInstallProposal(installProposalRequest, peersFromOrg);
+
+            for (ProposalResponse response : responses) {
+                if (response.getStatus() == ChaincodeResponse.Status.SUCCESS) {
+                    log.info("Successful install proposal response Txid: {} from peer {}", response.getTransactionID(), response.getPeer().getName());
+                    successful.add(response);
+                } else {
+                    failed.add(response);
+                }
+            }
+
+            log.info("Received {} install proposal responses. Successful+verified: {} . Failed: {}", numInstallProposal, successful.size(), failed.size());
+
+            if (failed.size() > 0) {
+                ProposalResponse first = failed.iterator().next();
+                log.error("Not enough endorsers for install :" + successful.size() + ".  " + first.getMessage());
+            }
+
+            // Check that all the proposals are consistent with each other. We should have only one set
+            // where all the proposals above are consistent.
+            Collection<Set<ProposalResponse>> proposalConsistencySets = SDKUtils.getProposalConsistencySets(responses);
+            if (proposalConsistencySets.size() != 1) {
+                log.error(format("Expected only one set of consistent install proposal responses but got {}", proposalConsistencySets.size()));
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
             UpgradeProposalRequest upgradeProposalRequest = hfClient.newUpgradeProposalRequest();
 
             upgradeProposalRequest.setChaincodeID(chaincodeID);
             upgradeProposalRequest.setProposalWaitTime(Long.parseLong(fabricConfigManager.getFabricConfig().getProposalWaitTime()));
             upgradeProposalRequest.setFcn("init");
-            upgradeProposalRequest.setArgs(new String[] {"c", "500"});
+            upgradeProposalRequest.setArgs(new String[] {});
             ChaincodeEndorsementPolicy chaincodeEndorsementPolicy = new ChaincodeEndorsementPolicy();
             chaincodeEndorsementPolicy.fromYamlFile(new File(config.getFixturesPath() + "/chaincodeendorsementpolicy.yaml"));
 
@@ -239,9 +305,9 @@ public class FabricService {
             upgradeProposalRequest.setTransientMap(tmap);
             upgradeProposalRequest.setUserContext(sampleOrg.getPeerAdmin());
 
-            Collection<ProposalResponse> responses = channel.sendUpgradeProposal(upgradeProposalRequest);
+            Collection<ProposalResponse> responses2 = channel.sendUpgradeProposal(upgradeProposalRequest);
 
-            for (ProposalResponse proposalResponse : responses) {
+            for (ProposalResponse proposalResponse : responses2) {
                 if(proposalResponse.getStatus().equals(ChaincodeResponse.Status.SUCCESS)) {
                     successful.add(proposalResponse);
                 } else {
