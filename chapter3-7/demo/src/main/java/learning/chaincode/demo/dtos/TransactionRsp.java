c400e7ab55c722adb5dc927b58c2d25a01f5c417
Roc0924 <1228916292@qq.com>
2018-03-05 18:25:21 +0800

    stripping install chaincode

---
+++ chapter3-7/demo/src/main/java/learning/chaincode/demo/dtos/TransactionRsp.java
@@ -0,0 +1,15 @@
+package learning.chaincode.demo.dtos;
+
+import lombok.Data;
+
+/**
+ * Create with IntelliJ IDEA
+ * Author               : wangzhenpeng
+ * Date                 : 2018/3/5
+ * Time                 : 15:20
+ * Description          :
+ */
+@Data
+public class TransactionRsp {
+    private String transactionID;
+}
