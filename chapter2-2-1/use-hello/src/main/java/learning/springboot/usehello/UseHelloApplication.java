021905f4d862b0228d49262c2e66563c379ea8d2
wangzhenpeng <1228916292@qq.com>
2018-03-12 11:35:33 +0800

    modify properties

--- chapter2-2-1/use-hello/src/main/java/learning/springboot/usehello/UseHelloApplication.java
+++ chapter2-2-1/use-hello/src/main/java/learning/springboot/usehello/UseHelloApplication.java
@@ -1,24 +1,10 @@
 package learning.springboot.usehello;
 
-import learning.springboot.HelloService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
 @SpringBootApplication
 public class UseHelloApplication {
-
-	@Autowired
-	private HelloService helloService;
-
-	@RequestMapping("/")
-	public String index() {
-		return helloService.sayHello();
-	}
-
 
 
 	public static void main(String[] args) {
